1a51d8018aba08ba60bf2bd42ee88a14fa795dbb
Aidan <aidankmcalister@gmail.com>
2024-01-09 10:36:02 -0600

    intermission

---
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -0,0 +1,34 @@
+import Article from 'src/components/Article'
+
+import type { ArticlesQuery } from 'types/graphql'
+import { Link, routes } from '@redwoodjs/router'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
+  return (
+    <ul>
+      {articles.map((article) => (
+        <Article key={article.id} article={article} />
+      ))}
+    </ul>
+  )
+}
