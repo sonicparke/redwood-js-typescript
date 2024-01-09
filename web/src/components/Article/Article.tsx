95776a84a146914ade6945ab87fab29bba810f89
Aidan <aidankmcalister@gmail.com>
2024-01-09 11:42:35 -0600

    chapter 5

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -1,19 +1,26 @@
-import { Link, routes } from "@redwoodjs/router"
-import type { Post } from "types/graphql"
+import { Link, routes } from '@redwoodjs/router'
+import type { Post } from 'types/graphql'
+
+const truncate = (text: string, length: number) => {
+  return text.substring(0, length) + '...'
+}
 
 interface Props {
   article: Post
+  summary?: boolean
 }
 
-const Article = ({ article }: Props) => {
+const Article = ({ article, summary = false }: Props) => {
   return (
     <article>
       <header>
-        <h2>
-          <Link to={routes.article({id:article.id})}>{article.title}</Link>
+        <h2 className="text-xl font-semibold text-blue-700">
+          <Link to={routes.article({ id: article.id })}>{article.title}</Link>
         </h2>
       </header>
-      <div>{article.body}</div>
+      <div className="mt-2 font-light text-gray-900">
+        {summary ? truncate(article.body, 100) : article.body}
+      </div>
       <div>Posted at: {article.createdAt}</div>
     </article>
   )
